b0358ea4289fa49d15314dff4ca34a92094057b3
Neo Racer <0xNeoRacer@protonmail.com>
2026-03-11 11:05:34 +0100

    full vault setup

--- silo-vaults/deploy/SiloVaultsSetup.s.sol
+++ silo-vaults/deploy/SiloVaultsSetup.s.sol
@@ -9,10 +9,17 @@ import {ISiloVault} from "silo-vaults/contracts/interfaces/ISiloVault.sol";
 import {ISiloVaultDeployer} from "silo-vaults/contracts/interfaces/ISiloVaultDeployer.sol";
 import {IIncentivesClaimingLogicFactory} from "silo-vaults/contracts/interfaces/IIncentivesClaimingLogicFactory.sol";
 import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
-import {ISiloIncentivesController} from "silo-core/contracts/incentives/interfaces/ISiloIncentivesController.sol";
+import {ISiloIncentivesController} from "silo-vaults/contracts/interfaces/ISiloIncentivesControllerCLDeployer.sol";
 import {CommonDeploy} from "./common/CommonDeploy.sol";
 import {IERC20Metadata} from "openzeppelin5/token/ERC20/extensions/IERC20Metadata.sol";
 import {IERC4626} from "openzeppelin5/interfaces/IERC4626.sol";
+import {IERC20} from "openzeppelin5/interfaces/IERC20.sol";
+import {
+    ISiloIncentivesControllerCLDeployer
+} from "silo-vaults/contracts/incentives/claiming-logics/SiloIncentivesControllerCLDeployer.sol";
+import {
+    SiloIncentivesControllerCL
+} from "silo-vaults/contracts/incentives/claiming-logics/SiloIncentivesControllerCL.sol";
 
 import {ChainsLib} from "silo-foundry-utils/lib/ChainsLib.sol";
 import {AddrLib} from "silo-foundry-utils/lib/AddrLib.sol";
@@ -28,17 +35,25 @@ This script sets up the vault for QA.
 */
 contract SiloVaultsSetup is CommonDeploy, StdCheats {
     function run() public {
+        AddrLib.init();
+
         ISiloVaultDeployer vaultDeployer = ISiloVaultDeployer(
             SiloVaultsDeployments.get(SiloVaultsContracts.SILO_VAULT_DEPLOYER, ChainsLib.chainAlias())
         );
 
-        AddrLib.init();
         address asset = AddrLib.getAddress(vm.envString("ASSET"));
         string memory assetSymbol = IERC20Metadata(asset).symbol();
         string memory vaultName = string.concat("Silo Vault for ", assetSymbol);
         string memory vaultSymbol = string.concat("SV-", assetSymbol);
 
         IIncentivesClaimingLogicFactory[] memory trustedFactories = new IIncentivesClaimingLogicFactory[](1);
+
+        trustedFactories[0] = IIncentivesClaimingLogicFactory(
+            SiloVaultsDeployments.get(
+                SiloVaultsContracts.SILO_INCENTIVES_CONTROLLER_CL_FACTORY, ChainsLib.chainAlias()
+            )
+        );
+
         ISilo[] memory silosWithIncentives = new ISilo[](0);
 
         uint256 privateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
@@ -49,25 +64,27 @@ contract SiloVaultsSetup is CommonDeploy, StdCheats {
 
         vm.startBroadcast(privateKey);
 
-        // (ISiloVault vault, ISiloIncentivesController incentivesController, IERC4626 idleVault) = vaultDeployer.createSiloVault(
-        //     ISiloVaultDeployer.CreateSiloVaultParams({
-        //         initialOwner: deployer,
-        //         initialTimelock: 0,
-        //         asset: asset,
-        //         incentivesControllerOwner: deployer,
-        //         name: vaultName,
-        //         symbol: vaultSymbol,
-        //         trustedFactories: trustedFactories,
-        //         silosWithIncentives: silosWithIncentives
-        //     })
-        // );
+        (ISiloVault vault, ISiloIncentivesController incentivesController, IERC4626 idleVault) = vaultDeployer.createSiloVault(
+            ISiloVaultDeployer.CreateSiloVaultParams({
+                initialOwner: deployer,
+                initialTimelock: 0,
+                asset: asset,
+                incentivesControllerOwner: deployer,
+                name: vaultName,
+                symbol: vaultSymbol,
+                trustedFactories: trustedFactories,
+                silosWithIncentives: silosWithIncentives
+            })
+        );
 
         vm.stopBroadcast();
 
-        ISiloVault vault = ISiloVault(0x980AdDAEC01E69E55d62bA105C6045D92B323745);
-        IERC4626 idleVault = IERC4626(0x8520865cdBAA712Ba74703a82d427e08c54422Da);
+        // ISiloVault vault = ISiloVault(0x980AdDAEC01E69E55d62bA105C6045D92B323745);
+        // IERC4626 idleVault = IERC4626(0x8520865cdBAA712Ba74703a82d427e08c54422Da);
 
         console2.log("Vault deployed at", address(vault));
+        console2.log("Incentives controller deployed at", address(incentivesController));
+        console2.log("Idle vault deployed at", address(idleVault));
 
         {
             uint256 decimals = IERC20Metadata(asset).decimals();
@@ -87,33 +104,69 @@ contract SiloVaultsSetup is CommonDeploy, StdCheats {
             vault.setSupplyQueue(supplyQueue);
 
             vm.stopBroadcast();
-
-            // TODO claiming logic for defaulting
-
         }
 
+        SiloIncentivesControllerCL logic;
+
+        {
+            ISiloIncentivesControllerCLDeployer controllerDeployer = ISiloIncentivesControllerCLDeployer(
+                SiloVaultsDeployments.get(
+                    SiloVaultsContracts.SILO_INCENTIVES_CONTROLLER_CL_DEPLOYER, ChainsLib.chainAlias()
+                )
+            );
+
+            IVaultIncentivesModule module = vault.INCENTIVES_MODULE();
+
+            vm.startBroadcast(privateKey);
+            logic = controllerDeployer.createIncentivesControllerCL(address(vault), address(silo));
+            module.submitIncentivesClaimingLogic(silo, logic);
+            module.acceptIncentivesClaimingLogic(silo, logic);
+            vm.stopBroadcast();
+
+            console2.log("Claiming logic deployed at", address(logic));
+        }
 
         vm.label(address(vault), "Vault");
         vm.label(address(silo), "Silo");
 
-        _qa(vault, silo);
+        _qa(vault, logic);
     }
 
-    function _qa(ISiloVault vault, IERC4626 silo) internal {
-        address asset = vault.asset();
+    function _qa(ISiloVault _vault, SiloIncentivesControllerCL _logic) internal {
+        address asset = _vault.asset();
         address depositor = address(0x11);
         uint256 asetDecimals = IERC20Metadata(asset).decimals();
 
         deal(asset, depositor, uint256(10 ** asetDecimals));
 
         vm.startPrank(depositor);
-        IERC4626(asset).approve(address(vault), 10 ** asetDecimals);
-        uint256 shares = vault.deposit(10 ** asetDecimals, depositor);
-        
+        IERC4626(asset).approve(address(_vault), 1000 * 10 ** asetDecimals);
+        uint256 shares = _vault.deposit(10 ** asetDecimals, depositor);
+
         console2.log("Shares deposited", shares);
 
-        uint256 assets = vault.redeem(shares, depositor, depositor);
+        uint256 assets = _vault.redeem(shares, depositor, depositor);
         console2.log("Assets redeemed", assets);
+
+        console2.log("QA Claiming logic");
+
+        ISiloIncentivesController siloIncentives = _logic.SILO_INCENTIVES_CONTROLLER();
+        ISiloIncentivesController vaultIncentives = _logic.VAULT_INCENTIVES_CONTROLLER();
+
+        shares = _vault.deposit(IERC20(asset).balanceOf(depositor), depositor);
+
+        uint256 amount = 10 ** asetDecimals;
+        deal(asset, address(siloIncentives), amount);
+        vm.stopPrank();
+
+        vm.prank(siloIncentives.NOTIFIER());
+        siloIncentives.immediateDistribution(asset, amount);
+
+        vm.startPrank(depositor);
+        assets = _vault.redeem(shares, depositor, depositor);
+        vaultIncentives.claimRewards(depositor);
+        require(IERC20(asset).balanceOf(depositor) > assets, "expect rewards");
+
         vm.stopPrank();
     }
 }
